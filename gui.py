d7a5e715c6e62464c4d627cb98d2306dbc45b37d
Artheek18 <artheek2321.Shan@gmail.com>
2025-08-28 21:01:52 -0400

    implemented the Edit button

--- gui.py
+++ gui.py
@@ -1,13 +1,19 @@
 import functions
 import FreeSimpleGUI as fs
 
-label = fs.Text("Type a to-do")
+label = fs.Text("Type a to-do to add")
 input_box = fs.InputText(tooltip="Enter a to-do", key="todo")
 add_button = fs.Button("Add To-Do")
 
+label_for_edit = fs.Text("Edit a To-Do")
+list_box = fs.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=(40, 20))
+edit_button = fs.Button("Edit To-Do")
+
 window = fs.Window("To-Do App",
-                      layout=[[label], [input_box, add_button]],
+                      layout=[[label], [input_box, add_button], [label_for_edit], [list_box, edit_button]],
                       font=("Helvetica bold", 10))
+
 
 while True:
     event, values = window.read()
@@ -19,6 +25,20 @@ while True:
             added = values['todo'] + "\n"
             todos.append(added)
             functions.write_todos(todos)
+
+        case "Edit To-Do":
+            edit_todo = values['todos'][0]
+            new = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(edit_todo)
+            todos[index] = new
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
+
 
         case fs.WINDOW_CLOSED:
             break
